6d22ea59a1117cd1fc0001492d1b6c9bfed1f802
John Ky <newhoggy@gmail.com>
2019-05-28 08:14:30 +1000

    MonoidalSpec stub

---
+++ test/HaskellWorks/Data/BalancedParens/Internal/MonoidalSpec.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+
+module HaskellWorks.Data.BalancedParens.Internal.MonoidalSpec where
+
+import Test.Hspec
+import Test.QuickCheck
+
+{-# ANN module ("HLint: Ignore Redundant do"        :: String) #-}
+{-# ANN module ("HLint: Ignore Reduce duplication"  :: String) #-}
+
+spec :: Spec
+spec = describe "HaskellWorks.Data.BalancedParens.Internal.MonoidalSpec" $ do
+  it "Stub" $ do
+    property $ \(w :: Word64) ->
+      w == w
