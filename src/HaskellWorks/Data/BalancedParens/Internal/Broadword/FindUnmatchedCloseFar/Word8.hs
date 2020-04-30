a3f26bed067a465df192332cff4f758a6b7dceda
John Ky <newhoggy@gmail.com>
2020-04-30 20:07:45 +1000

    Document variable names in broadword implementation

--- src/HaskellWorks/Data/BalancedParens/Internal/Broadword/FindUnmatchedCloseFar/Word8.hs
+++ src/HaskellWorks/Data/BalancedParens/Internal/Broadword/FindUnmatchedCloseFar/Word8.hs
@@ -31,6 +31,20 @@ muk2 = 0x0f
 -- See [Broadword Implementation of Parenthesis Queries](https://arxiv.org/pdf/1301.5468.pdf), Sebastiano Vigna, 2013
 findUnmatchedCloseFar :: Word64 -> Word8 -> Word64
 findUnmatchedCloseFar p w =
+  --  Keys:
+  --    * k1: Level of sub-words of size 2 = 1 .<. 1
+  --    * k2: Level of sub-words of size 4 = 1 .<. 2
+  --    * k3: Level of sub-words of size 8 = 1 .<. 3
+  --    * o: Open count
+  --    * c: Close count
+  --    * e: Excess
+  --    * L: Left half of sub-word
+  --    * R: Right half of sub-word
+  --    * b: deficit at position whole-word mask
+  --    * m: deficit at position sub-word mask
+  --    * pa: position accumulator
+  --    * sa: shift accumulator
+  --    * f: far sub-block close parens count
   let x     = w .>. p                                                                     in
   let wsz   = 8 :: Int8                                                                   in
   let k1    = 1                                                                           in
